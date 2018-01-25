6959ad0f3b0b8d2d5bc2c4dad75ff5f69470af1c
FlorianFrank <ffrank@hm.edu>
2018-01-26 00:11:15 +0100

    Add additional Tests, to check REST and database server functionality

--- test/RESTSpec.hs
+++ test/RESTSpec.hs
@@ -1,29 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 module RESTSpec (spec) where
 
+-- import internal modules
 import Model.RESTDatatypes
 
+
+--import external modules
 import Test.Hspec
 import Test.QuickCheck
 
-import Data.Int
-import Web.Spock.Config
-import Control.Monad.Logger
-import Database.Persist.Class
-import Database.Persist
-import Database.Persist.Sqlite
-import Database.Persist.TH
-import Web.Spock (spockAsApp, spock)
 
-spec:: Spec
+-- | Tests convertion functions of REST-Module
+spec :: Spec -- ^ Testspec
 spec =
     describe "toStringMethods" $ do
         it "dateToString tupel (1, 12, 2017) should be 12.01.2017" $
             dateToString (1, 12, 2017) `shouldBe` "12.01.2017"
         it "dateToString tupel (12, 31, 1978) should be 31.12.1978" $
             dateToString (12, 31, 1978) `shouldBe` "31.12.1978"
+        it "dateToString tupel (12, 31, 1978) should be 31.12.1978" $
+            dateToString (2, 29, 2016) `shouldBe` "29.02.2016"
+        it "dateToString tupel (12, 31, 1978) should be 31.12.1978" $
+            dateToString (1, 12, 1960) `shouldBe` "12.01.1960"
         it "timeToString tupel (22,33) should be 22:33" $
             timeToString (22, 33) `shouldBe` "22:33"
         it "timeToString tupel (3,0) should be 22:33" $
             timeToString (3, 0) `shouldBe` "03:00"
-        --it "membersReady [Member \"Max\" \"Mustermann\" 1 1 1990 30 1 2029 1 1 1, Member \"Petra\" \"Wagner\" 1 1 1990 30 1 2017 1 1 1]" $
+        it "timeToString tupel (2,33) should be 02:33" $
+            timeToString (2, 33) `shouldBe` "02:33"
+        it "timeToString tupel (0,0) should be 00:00" $
+            timeToString (0, 0) `shouldBe` "00:00"
--- test/WebSrvSpec.hs
+++ test/WebSrvSpec.hs
@@ -1,28 +1,22 @@
 {-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE EmptyDataDecls             #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE QuasiQuotes                #-}
-{-# LANGUAGE TemplateHaskell            #-}
-{-# LANGUAGE TypeFamilies               #-}
+-- | Module Tests Web and RESTServices such as the Database
 module WebSrvSpec (spec) where
 
-import Data.String
-import Control.App
+-- import internal modules
+import Control.App (app)
 import Model.Config as C
-import Test.Hspec
-import Test.Hspec.Wai
-import Test.Hspec.Wai.JSON as J
-import Test.Hspec.Wai.Matcher
-import Web.Spock.Config
-import Control.Monad.Logger
-import Database.Persist.Sqlite hiding (get,post)
-import Web.Spock hiding (get,post)--(spockAsApp, spock)
-import qualified Data.ByteString.Lazy.Char8 as L
 
+-- import external modules
+import                      Test.Hspec
+import                      Test.Hspec.Wai
+import                      Test.Hspec.Wai.JSON as J
+import                      Test.Hspec.Wai.Matcher
+import                      Web.Spock.Config
+import                      Control.Monad.Logger
+import                      Database.Persist.Sqlite hiding (get,post)
+import                      Web.Spock hiding (get,post)
+import qualified            Data.ByteString.Lazy.Char8 as L
 
 
 getConfig cfg =
@@ -31,8 +25,8 @@ getConfig cfg =
        spockAsApp $ spock spockCfg app
 
 
-
-spec :: Spec
+-- | Tests REST and Database Functions
+spec :: Spec -- ^ Testspec
 spec =
    with (getConfig (Config "test.db" 8080)) $
        do describe "GET /createTable" $
@@ -58,12 +52,74 @@ spec =
           describe "GET /register" $
                 do it "Test if register is available!" $ do
                         get "/register" `shouldRespondWith`  200
-          describe "POST /register" $
+          describe "POST register" $
                 do it "Test if registration works! (Set mail:\"Test@test.de\", password: \"Test\")" $ do
                         post "register" [J.json|{mail: "Test@test.de" ,password: "Test"}|] `shouldRespondWith`  "{\"result\":\"success\",\"id\":1}" {matchStatus = 200}
-          describe "POST /login" $
+          describe "POST login" $
                 do it "Test if login is impossible when using false password! (Get mail:\"Test@test.de\", password: \"Test2\")" $ do
                         post "login" [J.json|{mail: "Test@test.de" ,password: "Test2"}|] `shouldRespondWith`  "{\"error\":{\"code\":2,\"message\":\"Could not find a person with matching id\"},\"result\":\"failure\"}" {matchStatus = 200}
-          describe "POST /login" $
+          describe "POST login" $
                 do it "Test if login is possible! (Get mail:\"Test@test.de\", password: \"Test\")" $ do
                         post "login" [J.json|{mail: "Test@test.de" ,password: "Test"}|] `shouldRespondWith` "{\"password\":\"Test\",\"id\":1,\"mail\":\"Test@test.de\"}" {matchStatus = 200}
+          describe "POST addMember" $
+                do it "Test addMember Works with {name: \"Mustermann\" ,surName: \"Max\", birthDay: 31, \
+                            \ birthMonth: 8, birthYear: 1993, examationDay: 1, examationMonth: 2, examationYear: 2019, \
+                             \ instructionCheck: 0, exerciseCheck: 0}" $ do
+                        post "addMember" [J.json|{name: "Mustermann" ,surName: "Max", birthDay: 31,
+                            birthMonth: 8, birthYear: 1993, examationDay: 1, examationMonth: 2, examationYear: 2019,
+                            instructionCheck: 0, exerciseCheck: 0}|]
+                                `shouldRespondWith` "{\"result\":\"success\",\"id\":1}" {matchStatus = 200}
+          describe "POST addMember" $
+                do it "Test if addMember Works with {name: \"Mustermann\" ,surName: \"Petra\", birthDay: 21, \
+                            \ birthMonth: 12, birthYear: 2000, examationDay: 2, examationMonth: 2, examationYear: 2017, \
+                            \ instructionCheck: 0, exerciseCheck: 0}" $ do
+                      post "addMember" [J.json|{name: "Mustermann" ,surName: "Petra", birthDay: 21,
+                          birthMonth: 12, birthYear: 2000, examationDay: 2, examationMonth: 2, examationYear: 2017,
+                          instructionCheck: 0, exerciseCheck: 0}|]
+                              `shouldRespondWith` "{\"result\":\"success\",\"id\":2}" {matchStatus = 200}
+          describe "POST addMember" $
+                do it "Test if addMember Works with {name: \"Wagner\" ,surName: \"Katja\", birthDay: 27, \
+                            \ birthMonth: 1, birthYear: 1993, examationDay: 4, examationMonth: 1, examationYear: 2019, \
+                            \ instructionCheck: 1, exerciseCheck: 1}" $ do
+                      post "addMember" [J.json|{name: "Wagner" ,surName: "Katja", birthDay: 27,
+                          birthMonth: 1, birthYear: 1993, examationDay: 4, examationMonth: 1, examationYear: 2019,
+                          instructionCheck: 1, exerciseCheck: 1}|]
+                              `shouldRespondWith` "{\"result\":\"success\",\"id\":3}" {matchStatus = 200}
+          describe "POST addMember" $
+                do it "Test if addMember Works with {name: \"Frank\" ,surName: \"Florian\", birthDay: 31, \
+                            \ birthMonth: 8, birthYear: 1993, examationDay: 4, examationMonth: 1, examationYear: 2019, \
+                            \ instructionCheck: 1, exerciseCheck: 1}" $ do
+                      post "addMember" [J.json|{name: "Fank" ,surName: "Florian", birthDay: 31,
+                          birthMonth: 8, birthYear: 1993, examationDay: 22, examationMonth: 11, examationYear: 2020,
+                          instructionCheck: 1, exerciseCheck: 1}|]
+                              `shouldRespondWith` "{\"result\":\"success\",\"id\":4}" {matchStatus = 200}
+          describe "POST deleteMember" $
+                do it "Test if deleteMember Works with {members: [{name: \"Frank\" ,surName: \"Florian\", birthDay: 31, \
+                            \ birthMonth: 8, birthYear: 1993, examationDay: 4, examationMonth: 1, examationYear: 2019, \
+                            \ instructionCheck: 1, exerciseCheck: 1}]}" $ do
+                      post "deleteMember" [J.json|{members: [{name: "Fank" ,surName: "Florian", birthDay: 31,
+                          birthMonth: 8, birthYear: 1993, examationDay: 22, examationMonth: 11, examationYear: 2020,
+                          instructionCheck: 1, exerciseCheck: 1}]}|]
+                              `shouldRespondWith` "{\"result\":\"success\",\"id\":[]}" {matchStatus = 200}
+          describe "POST deleteMember" $
+                do it "Test if deleteMember Works with {name: \"Wagner\" ,surName: \"Katja\", birthDay: 27, \
+                            \ birthMonth: 1, birthYear: 1993, examationDay: 4, examationMonth: 1, examationYear: 2019, \
+                            \ instructionCheck: 1, exerciseCheck: 1}" $ do
+                      post "deleteMember" [J.json|{members: [{name: "Wagner" ,surName: "Katja", birthDay: 27,
+                          birthMonth: 1, birthYear: 1993, examationDay: 4, examationMonth: 1, examationYear: 2019,
+                          instructionCheck: 1, exerciseCheck: 1}]}|]
+                              `shouldRespondWith` "{\"result\":\"success\",\"id\":[]}" {matchStatus = 200}
+          describe "POST addAppointment" $
+                do it "Test if addAppointment Works with {title: \"Test\" ,type: \"Übung\", day: 27, \
+                            \ month: 9, year: 2017, hour: 19, minute: 0, members: []}" $ do
+                      post "addAppointment" [J.json|{title: "Test", type: "Übung", day: 27,
+                          month: 9, year: 2017, hour: 19, minute: 0, members: []}|]
+                              `shouldRespondWith` "{\"result\":\"success\",\"id\":1}" {matchStatus = 200}
+    --      describe "POST addAppointment" $
+    --            do it "Test if addAppointment Works with {title: \"Test\" ,type: \"Uebung\", day: 27, \
+    --                        \ month: 9, year: 2017, hour: 19, minute: 0, members: [{name: \"Frank\" ,surName: \"Florian\", birthDay: 31, \
+    --                        \ birthMonth: 8, birthYear: 1993, examationDay: 22, examationMonth: 11, examationYear: 2020, \
+    --                        \ instructionCheck: 1, exerciseCheck: 1}]}" $ do
+    --                  post "addAppointment" [J.json|{title: "Test", type: "Uebung", day: 27,
+    --                      month: 9, year: 2017, hour: 19, minute: 0, members: ["Florian Frank"]}|]
+    --                            `shouldRespondWith` "{\"result\":\"success\",\"id\":1}" {matchStatus = 200}
